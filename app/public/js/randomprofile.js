87b67c6cd5502252aff9020ca5df630faebdd160
Riley McGrath <rpmcgrat@iu.edu>
2019-09-15 22:38:19 -0400

    add empty JSON structure

--- app/public/js/randomprofile.js
+++ app/public/js/randomprofile.js
@@ -1,15 +1,65 @@
 var waitingApp = new Vue({
   el: '#patientWaitingApp',
   data: {
-    person: [
+    person:
       {
+        "gender": "",
+        "name": {
+          "title": "",
+          "first": "",
+          "last": ""
+        },
+        "location": {
+          "street": "",
+          "city": "",
+          "state": "",
+          "postcode": "",
+          "coordinates": {
+            "latitude": "",
+            "longitude": ""
+          },
+          "timezone": {
+            "offset": "",
+            "description": ""
+          }
+        },
+        "email": "",
+        "login": {
+          "uuid": "",
+          "username": "",
+          "password": "",
+          "salt": "",
+          "md5": "",
+          "sha1": "",
+          "sha256": ""
+        },
+        "dob": {
+          "date": "",
+          "age": null
+        },
+        "registered": {
+          "date": "",
+          "age": null
+        },
+        "phone": "",
+        "cell": "",
+        "id": {
+          "name": "",
+          "value": ""
+        },
+        "picture": {
+          "large": "",
+          "medium": "",
+          "thumbnail": ""
+        },
+        "nat": ""
       }
-    ]
   },
+
   methods: {
     fetchPatients() {
       fetch('https://randomuser.me/api/')
-      .then(response => response.json())
+      .then(response => response.json() )
       .then(json => waitingApp.person = json.results[0]);
 
       // Means the same at this
@@ -19,6 +69,7 @@ var waitingApp = new Vue({
 
     }
   },
+
   created() {
     this.fetchPatients()
   }
